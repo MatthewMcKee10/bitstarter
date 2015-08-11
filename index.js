bc3bad3afc1c9a20226e0894fb94cd840168590e
Matthew McKee <ubuntu@ip-172-31-40-221.us-west-2.compute.internal>
2015-08-11 10:22:19 +0000

    Added readFileSync

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
+var fs = require('fs')
 
+fs.readFileSync('index.html')
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
