963f7d2645b61db124f984a4e229da984c9be376
Matthew McKee <ubuntu@ip-172-31-40-221.us-west-2.compute.internal>
2015-08-11 11:53:45 +0000

    Changed port from 5000 to 8080

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 var buf = fs.readFileSync('index.html','utf-8');
 var string = buf.toString();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
